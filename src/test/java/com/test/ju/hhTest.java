b9f6eafc8746185ea47dab70dfc8759c399eb0d1
starwang <starwang@sohu-inc.com>
2014-03-17 13:40:20 +0800

    0317

---
+++ src/test/java/com/test/ju/hhTest.java
@@ -0,0 +1,23 @@
+package com.test.ju;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class hhTest {
+
+
+
+@Test
+public void gh(){
+	Assert.assertEquals("e", "4e");;
+	//Assert.assertFalse(true);
+	System.out.print("hhh");
+}
+@Test
+public void retetgh(){
+	System.out.print("hhgh");
+}
+
+}
---
+++ src/test/java/com/test/ju/test.java
@@ -0,0 +1,7 @@
+package com.test.ju;
+
+import junit.framework.TestCase;
+
+public class test extends TestCase {
+
+}
